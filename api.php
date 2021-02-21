a7d5f87d0c5f5d22ee8863c0ddd5d3d4a3e285b5
LWANGYONG <47111189+LWANGYONG@users.noreply.github.com>
2021-02-21 22:09:12 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 
 $rand = rand(91,240);
-$url = 'https://cdn.jsdelivr.net/gh/LWANGYONG/cdn/img/'.$rand.'.jpg';
+$url = 'https://cdn.jsdelivr.net/gh/LWANGYONG/cdn/imgs/'.$rand.'.jpg';
 $img = file_get_contents($url,true);
 header("Content-Type: image/jpeg;");
 echo $img;
